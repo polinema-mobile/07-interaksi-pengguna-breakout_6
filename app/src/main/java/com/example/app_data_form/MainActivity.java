f6fef2f34503b64f7f28eb5b9fdcbe41a174518a
Mirza Rayhan <56281890+mirzarayhan@users.noreply.github.com>
2020-09-29 09:46:36 +0700

    Update Tampilan

--- app/src/main/java/com/example/app_data_form/MainActivity.java
+++ app/src/main/java/com/example/app_data_form/MainActivity.java
@@ -2,7 +2,15 @@ package com.example.app_data_form;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
